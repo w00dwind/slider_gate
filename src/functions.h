6862de026998abf860efdcfb46d1db82af6d9227
ac1d <ac1d@MacBook-Pro-ac1d-2.local>
2020-10-04 15:55:41 +0300

    v 1.0 Advanced logger and timer

---
+++ src/functions.h
@@ -0,0 +1,59 @@
+
+/*
+  Date and time functions
+*/
+String getCurrentTime() {
+  String extraZeroH, extraZeroM, extraZeroS;
+  if (hour() < 10) extraZeroH = '0';
+  if (minute() < 10) extraZeroM = '0';
+  if (second() < 10) extraZeroS = '0';
+  return String(extraZeroH + hour()) + ':' + extraZeroM + minute() + ':' + extraZeroS + second();
+}
+
+String getCurrentDate() {
+  return(String(day())) + "-" + monthShortStr(month()) + "-" + year();
+}
+
+void printTimeDate() {
+  terminal.println("-----------------------");
+  terminal.println( getCurrentDate() + String(" | ") + getCurrentTime() );
+}
+
+void sendUptime ()
+{
+  uptime::calculateUptime();
+  String uptime_format = (String)(uptime::getDays()) + "d:" + (String)(uptime::getHours()  ) + "h:" + (String)(uptime::getMinutes()) + "m" ;
+  Blynk.virtualWrite(vPIN_UPTIME,  uptime_format);
+}
+BLYNK_CONNECTED() {
+  // Synchronize time on connection
+  rtc.begin();
+  setSyncInterval(10 * 60); // Sync interval in seconds (10 minutes)
+}
+
+/*
+  printOutput() - easy print to terminal with date function
+*/
+void printOutput(String a) {
+  printTimeDate();
+  terminal.println(a);
+  terminal.flush();
+}
+
+
+/*
+  formatTime() - Format millis in to MM:SS
+*/
+String formatTime(long milliSeconds) {
+  long hours = 0, mins = 0, secs = 0;
+  String returned, secs_o, mins_o;
+  secs = milliSeconds / 1000;
+  mins = secs / 60;
+  hours = mins / 60;
+  secs = secs - (mins * 60);
+  mins = mins - (hours * 60);
+  if (secs < 10 && mins) secs_o = "0";
+  if (mins) returned += mins + String("m ");
+  returned += secs_o + secs + String("s");
+  return returned;
+}
---
+++ src/settings.h
@@ -0,0 +1,60 @@
+
+//
+#define DEBUG_MODE 1  // if enable - debug token
+#define LOCATION 1     // 1 =  Chistova.st ; 2 = Svyatoozerskaya; 3 = Shatura
+//
+
+/**************************************************************
+
+                            Pins
+
+ **************************************************************/
+ // ESP pins
+ #define PIN_MAGNET D7   // Physical  pin to a3144 sensor
+ #define PIN_RELAY1 D6    // Physical  pin to Relay for full open
+ #define PIN_RELAY2 D5   // Physical  pin to Relay short open
+
+// Virtual pins
+#define vPIN_TERMINAL               V21
+#define vPIN_CUR_DATE               V8
+#define vPIN_LCD                    V3
+
+#define vPIN_UPTIME                 V5     // uptime pin
+//#define BL_RSSI V6      // signal strength pin
+#define vPIN_RELAY1                 V1    //  full open gate
+#define vPIN_RELAY2                 V2   // short open gate
+
+
+#define vPIN_TEST_BTN               V20
+
+#define vPIN_GATE_TABLE_CLR         V13
+#define vPIN_GATE_TABLE             V23
+#define vPIN_GATE_LAST              V11
+#define vPIN_GATE_HELD              V14
+#define vPIN_GATE_COUNTER           V12
+
+
+/**************************************************************
+
+                            Globals
+
+ **************************************************************/
+WidgetRTC rtc;
+WidgetLCD lcd(vPIN_LCD);
+WidgetTerminal terminal(vPIN_TERMINAL);
+
+
+
+int timer1, timer2, timer3;
+BlynkTimer timer;
+int today;
+
+String GateLastOpened;
+int           DoorBellButtonCur, GateSwitchCurrent, notificationSent;
+long          GateSwitchMillisHeld, GateSwitchSecsHeld, notifyDelay;
+unsigned long GateSwitchFirstTime;       // how long since the button was first pressed
+byte          GateSwitchPrev = LOW;
+
+int GateDailyCounter;
+
+int           tableIndex1 = 0, tableIndex2 = 0;
